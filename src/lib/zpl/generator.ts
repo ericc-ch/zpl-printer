dd2ef21d5ba44b8a3aaa96316df6faaf5da13b5c
Erick Christian <erickchristian48@gmail.com>
2025-10-23 12:39:14 +0700

    feat: add ZPL label generation library with constants and generator

---
+++ src/lib/zpl/generator.ts
@@ -0,0 +1,67 @@
+import {
+  LABEL_SPECS,
+  calculateColumnXOffset,
+  calculatePrintWidth,
+  calculateLabelLength,
+} from "./constants";
+
+export interface LabelData {
+  qrCode: string;
+  line1: string;
+  line2: string;
+  line3: string;
+}
+
+export interface GenerateZplOptions {
+  labels: LabelData[];
+  columns: number;
+}
+
+function chunkArray<T>(array: T[], size: number): T[][] {
+  const chunks: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
+
+export function generateZpl(options: GenerateZplOptions): string {
+  const { labels, columns } = options;
+  const rows = chunkArray(labels, columns);
+  
+  const zplBlocks = rows.map((rowLabels) => generateRowZpl(rowLabels, columns));
+  
+  return zplBlocks.join('');
+}
+
+function generateRowZpl(rowLabels: LabelData[], columns: number): string {
+  const printWidth = calculatePrintWidth(columns);
+  const labelLength = calculateLabelLength();
+  
+  let zpl = '^XA\n';
+  zpl += `^PW${printWidth}\n`;
+  zpl += `^LL${labelLength}\n`;
+  zpl += '^LH0,0\n';
+  zpl += `^CI${LABEL_SPECS.ENCODING}\n`;
+  
+  rowLabels.forEach((label, columnIndex) => {
+    const xOffset = calculateColumnXOffset(columnIndex);
+    zpl += generateLabelZpl(label, xOffset);
+  });
+  
+  zpl += '^PQ1,0,1,Y\n';
+  zpl += '^XZ\n';
+  
+  return zpl;
+}
+
+function generateLabelZpl(label: LabelData, xOffset: number): string {
+  let zpl = '';
+  
+  zpl += `^FO${xOffset + 20},19^BQN,2,${LABEL_SPECS.QR_MAGNIFICATION}^FDQA,${label.qrCode}^FS\n`;
+  zpl += `^FO${xOffset + 190},34^A${LABEL_SPECS.TEXT_FONT}^FD${label.line1}^FS\n`;
+  zpl += `^FO${xOffset + 190},74^A${LABEL_SPECS.TEXT_FONT}^FD${label.line2}^FS\n`;
+  zpl += `^FO${xOffset + 190},114^A${LABEL_SPECS.TEXT_FONT}^FD${label.line3}^FS\n`;
+  
+  return zpl;
+}
